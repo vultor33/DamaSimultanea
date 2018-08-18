8847a5b6226863648bf8f58dfb850ab22bb64ea3
Vultor <fred.vultor@gmail.com>
2018-08-18 18:45:12 -0300

    enabled positions

--- app/src/main/java/com/example/android/damasimultanea/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/damasimultanea/MyRecyclerViewAdapter.java
@@ -6,18 +6,30 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
 
-    private String[] mData = new String[0];
+    private int[] enabledPositions = {
+            0,2,4,6,
+            9,11,13,15,
+            16,18,20,22,
+            25,27,29,31,
+            32,34,36,38,
+            41,43,45,47,
+            48,50,52,54,
+            57,59,61,63};
+
+    int numberOfElements = 64;
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
 
     // data is passed into the constructor
-    MyRecyclerViewAdapter(Context context, String[] data) {
+    MyRecyclerViewAdapter(Context context) {
         this.mInflater = LayoutInflater.from(context);
-        this.mData = data;
     }
 
     // inflates the cell layout from xml when needed
@@ -30,24 +42,31 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     // binds the data to the textview in each cell
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        if(mData[position] == "0")
+        if (isPlayablePosition(position)) {
             holder.myTextView.setBackgroundColor(Color.BLACK);
+            holder.pieceImage.setVisibility(View.VISIBLE);
+        } else {
+            holder.myTextView.setBackgroundColor(Color.WHITE);
+            holder.pieceImage.setVisibility(View.INVISIBLE);
+        }
     }
 
     // total number of cells
     @Override
     public int getItemCount() {
-        return mData.length;
+        return numberOfElements;
     }
 
 
     // stores and recycles views as they are scrolled off screen
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView myTextView;
+        ImageView pieceImage;
 
         ViewHolder(View itemView) {
             super(itemView);
             myTextView = (TextView) itemView.findViewById(R.id.rv_item_text);
+            pieceImage = (ImageView) itemView.findViewById(R.id.rv_item_circle);
             itemView.setOnClickListener(this);
         }
 
@@ -59,7 +78,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
     // convenience method for getting data at click position
     String getItem(int id) {
-        return mData[id];
+        return String.valueOf(id);
     }
 
     // allows clicks events to be caught
@@ -71,4 +90,13 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
     public interface ItemClickListener {
         void onItemClick(View view, int position);
     }
+
+    boolean isPlayablePosition(int position){
+        for (int enabled : enabledPositions){
+            if(position == enabled)
+                return true;
+        }
+        return false;
+    }
+
 }
