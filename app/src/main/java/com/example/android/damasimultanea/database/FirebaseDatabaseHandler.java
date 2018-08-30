a8a6d9f3cad2650b5a994d9aa077b60d23c1b05e
Vultor <fred.vultor@gmail.com>
2018-08-30 16:09:24 -0300

    working

--- app/src/main/java/com/example/android/damasimultanea/database/FirebaseDatabaseHandler.java
+++ app/src/main/java/com/example/android/damasimultanea/database/FirebaseDatabaseHandler.java
@@ -68,7 +68,6 @@ public class FirebaseDatabaseHandler {
         setMenuPlayTitle();
     }
 
-
     public void updateGameStatus(GameHandler gameChanges){
         mGameHandlerReference.child(gameKey).setValue(gameChanges);
     }
